9422657377a415f70df4152c72eb2e5983bc894b
Sai Krishna Ponnam <ponnam2@wisc.edu>
2025-05-14 23:49:24 -0500

    feat: tanh

---
+++ src/layers/tanh.py
@@ -0,0 +1,22 @@
+import torch
+
+from src.layers.base_layer import BaseLayer
+
+
+class Tanh(BaseLayer):
+
+
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: torch.Tensor):
+        exp_x = torch.exp(-2 * x)
+        self.output = (1 - exp_x) / (1 + exp_x)
+        return self.output
+
+    def backward(self, d_out: torch.Tensor):
+        return d_out * (1 - self.output**2)
+
+    def get_params(self):
+        return []
